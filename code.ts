0c0f45d1cef1b83638e283824efd6b215c456ae4
11ife <11ife>
2022-04-05 19:45:59 +0200

    creating new files for calculator

---
+++ code.ts
@@ -0,0 +1,6 @@
+let displayOperations = document.querySelector(".operations");
+let displayResult = document.querySelector(".result");
+
+if(displayOperations.value == null){
+    displayResult.innerHTML = "0";
+}
