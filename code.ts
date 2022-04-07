5dc756bbcedbfb52477e0f582ce2eb3a347c5004
11ife <hectorrdez@gmail.com>
2022-04-07 13:22:04 +0200

    big changes

--- code.ts
+++
@@ -1,6 +0,0 @@
-let displayOperations = document.querySelector(".operations");
-let displayResult = document.querySelector(".result");
-
-if(displayOperations.value == null){
-    displayResult.innerHTML = "0";
-}
